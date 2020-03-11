a11235f7ac0828460af59e7e88db513f98930257
Enayatullah Norozi <enayathushmand12@gmail.com>
2020-03-12 00:17:14 +0100

    removing junk

---
+++ formelblad_app/lib/data/scripts.dart
@@ -0,0 +1,38 @@
+// Copyright 2015 the Dart project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file.
+/* 
+void main() {
+  List formler = [
+    "k",
+    "kx+C",
+    "x^n (n\neq -1)",
+    "\frac{x^{n+1}}{n+1}+C",
+    "\frac{1}{x}(\,x\neq 0)",
+    "ln |x|+C",
+    "e^x",
+    "e^x+C",
+    "e^{kx}",
+    "\frac{e^{kx}}{k}+C",
+    "a^x \,(a>0;a\neq1)",
+    "\frac{a^x}{ln\,a}+C",
+    "sin\, x",
+    "-cos\,x+C",
+    "cos\, x",
+    "sin\, x+ C",
+    "sin\,kx",
+    "\frac{-cos\, kx}{k}+C",
+    "cos\, kx",
+    "\frac{sin\, kx}{k}+C"
+  ];
+
+  int nums = 0;
+
+  for (int i = 0; i < formler.length; i++) {
+    print(i % 2 == 0 ? "<tr>" : "");
+    print("<td>&nbsp;\\(" + formler[i] + "\\)</td>");
+    nums += 1;
+    print(nums % 2 == 0 ? "</tr>" : "");
+  }
+}
+ */
